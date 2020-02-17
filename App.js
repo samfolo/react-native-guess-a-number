8448f41f0719c91ab2fc3cf1a72e601293da9d0f
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-17 20:49:45 +0000

    feat: create <Header /> component and begin <StartGameScreen /> component

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import styles from './App.styles';
+
+import Header from './components/Header/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/StartGameScreen/StartGameScreen.styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  }
+});
+
+export default styles;
